75a38097f73af5e6f1b1e8687a74400724d444ca
Emaad <emadfredev@gmail.com>
2024-11-24 13:04:46 +0330

    Created signup route for backend and handeled error on it

---
+++ src/app/api/auth/signup/route.js
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+
+import User from "@/utils/connectDB"
+import connectDB from "@/utils/connectDB";
+import { hashPassword } from "@/utils/auth";
+
+export async function POST(req) {
+    try {
+        await connectDB();
+
+        const { email, password } = await req.json();
+        console.log({ email, password });
+
+        if (!email || !password) {
+            return NextResponse.json({
+                error: "اطلاعات را به درستی وارد کنید"
+            },
+                {
+                    status: 422
+                })
+        }
+        const existingUser = await User.findOne({ email });
+        console.log(existingUser);
+        if(existingUser) {
+            return NextResponse.json({
+                error:" این حساب کاربری وجود دارد "},{
+                    status:500,
+                })
+        }
+        const hashedPassword = await hashPassword(password);
+
+        const newUser = await User.create({ email, password: hashedPassword });
+
+        return NextResponse.json({message: "حساب کاربری ایجاد شد"})
+
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json({ error: " مشکلی در سرور رخ داده است" },
+            {
+                status: 500,
+            }
+        )
+    }
+}
