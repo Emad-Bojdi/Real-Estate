ef343dee8a27d9a00b5d3dcaac0981eb8aa18ec9
Emaad <emadfredev@gmail.com>
2024-12-11 12:12:04 +0330

    Created Profile model for Creating advertisment

---
+++ src/models/Profile.js
@@ -0,0 +1,55 @@
+import { Schema, model, models } from "mongoose";
+
+const profileSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    realState: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    constructionDate: {
+        type: Date,
+        required: true
+    },
+    category: {
+        type: String,
+        enum: ["villa", "apartment", "store", "office"],
+        required: true,
+    },
+    amenities: {
+        type: [String],
+        default: []
+    },
+    amenities: {
+        type: [String],
+        default: []
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {
+    timeStamp: true
+});
+
+const profile = models.Profile || model("Profile", profileSchema);
+
+export default profileSchema;
