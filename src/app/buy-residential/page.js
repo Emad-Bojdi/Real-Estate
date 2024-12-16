aea902c371ff84c38bb055cd86f8dc58996b86ee
Emaad <emadfredev@gmail.com>
2024-12-16 21:03:49 +0330

    Developed category feature for sorting profiles

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,12 +1,17 @@
 import BuyResidentialPage from "@/template/BuyResidentialPage";
 
-async function BuyResidential() {
+async function BuyResidential({searchParams}) {
   const res = await fetch("http://localhost:3000/api/profile", { cache: "no-store" });
   const data = await res.json();
 
   if(data.error) return <h3>مشکلی پیش امده است</h3>
+
+  let finalData = data.data;
+  if(searchParams.category){
+    finalData = finalData.filter(i => i.category === searchParams.category)
+  }
   return (
-    <BuyResidentialPage data={data.data} />
+    <BuyResidentialPage data={finalData} />
   )
 }
 
