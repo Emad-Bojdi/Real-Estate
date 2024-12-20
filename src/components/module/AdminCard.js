1a1d33d2acb121071f28223388c075adf5fc88ab
Emaad <emadfredev@gmail.com>
2024-12-21 00:49:42 +0330

    Created a module for clean code

--- src/components/module/AdminCard.js
+++ src/components/module/AdminCard.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import styles from "@/module/AdminCard.module.css"
+import { sp } from "@/utils/replaceNumber"
 
-const AdminCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const AdminCard = ({ data: { title, description, location, price } }) => {
+    return (
+        <div className={styles.container}>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <div className={styles.properties}>
+                <span >{location}</span>
+                <span>{sp(price)}</span>
+            </div>
+        </div>
+    )
 }
 
 export default AdminCard
